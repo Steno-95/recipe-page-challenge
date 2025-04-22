0d2bc5f2a5a7dcccad171d701e8cb74c9809ef6a
Steno-95 <lezzistefano95@gmail.com>
2025-04-22 15:35:04 +0200

    Improved accessibility for the footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
-    <footer className="text-center text-[1.1em] md:text-[1.5em] text-amber-50 mt-auto">
+    <footer className="text-center text-[1.1em] md:text-[1.5em] text-[var(--text-color)] mt-auto">
       Challenge by{" "}
       <a
-        className="text-blue-800 hover:text-[var(--text-green)] focus:text-[var(--text-green)]"
+        className="text-blue-800 hover:text-[var(--text-dark)] focus:text-[var(--text-dark)]"
         href="https://www.frontendmentor.io?ref=challenge"
         target="_blank"
       >
@@ -11,7 +11,7 @@ function Footer() {
       </a>
       . Coded by{" "}
       <a
-        className="text-blue-800 hover:text-[var(--text-green)] focus:text-[var(--text-green)]"
+        className="text-blue-800 hover:text-[var(--text-dark)] focus:text-[var(--text-dark)]"
         href="https://github.com/Steno-95"
         title="The link will redirect to the github of Stefano Lezzi"
       >
